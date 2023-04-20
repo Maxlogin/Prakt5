3db8535eb39ba8da737e6764f2243db8e4a77beb
Maxlogin <Rustik@ru.com>
2023-04-20 12:55:26 +0300

    Готовое задание 4

---
+++ Zadanie_4/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie_4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            int n = int.Parse(txtNumber1.Text);
+            double a = double.Parse(txtNumber2.Text);
+            double sum = 1;
+            double term = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                term *= a;
+                sum += term;
+            }
+            lblResult.Content = $"Сумма={sum}";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
